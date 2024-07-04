803201f154a0e1460ace5e30adf0f79747b80fef
Hasona <bekoahmed20@gmail.com>
2024-07-04 21:46:47 +0300

    further fix

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Module to implement an annotated function that return the params in another shape"""
+"""Module to implement an annotated function
+that return the params in another shape"""
 
 
 from typing import Union, Tuple
@@ -7,4 +8,4 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns the string and the square of the float in a tuple"""
-    return (k, float(v*v))
+    return k, float(v*v)
