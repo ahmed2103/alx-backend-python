7f49046eb17adf105a46a64d9f211f932ecfc91e
hasona <bekoahmed20@gmail.com>
2025-09-18 18:07:00 +0300

    views

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
-from ..messaging_app.urls import urlpatterns
 
 router = DefaultRouter()
 
