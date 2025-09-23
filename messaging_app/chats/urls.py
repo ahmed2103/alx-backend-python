3bc262925fa43ddfbc82a1183a7ab18975ba0016
hasona <bekoahmed20@gmail.com>
2025-09-23 17:01:55 +0300

    task 0

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,16 +1,17 @@
+from django.contrib import admin
 from django.urls import path, include
 from rest_framework_nested import routers
 from .views import ConversationViewSet, MessageViewSet
 
-# Main router
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 
-# Nested router for messages under conversations
 conversations_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
 conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('', include(conversations_router.urls)),
+    path("admin/", admin.site.urls),
+    path("", include(router.urls)),
+    path("", include(conversations_router.urls)),
+    path("", include("chats.auth")),
 ]
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -4,19 +4,22 @@ from rest_framework import viewsets, permissions, status, filters
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from .permissions import IsParticipantOrOwner
+
 
 class ConversationViewSet(viewsets.ModelViewSet):
     """
     ViewSet for listing conversations and creating new ones
     """
     serializer_class = ConversationSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsParticipantOrOwner]
 
 
     def get_queryset(self):
         """
         Filter conversations to only show those where the current user is a participant
         """
+
         user = self.request.user
         return Conversation.objects.filter(participants=user).prefetch_related('messages__sender')
 
@@ -36,7 +39,7 @@ class MessageViewSet(viewsets.ModelViewSet):
     ViewSet for listing messages and sending new ones
     """
     serializer_class = MessageSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsParticipantOrOwner]
     filter_backends = [DjangoFilterBackend,filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['message_body', 'sender__first_name', 'sender__last_name']
     ordering_fields = ['sent_at']
