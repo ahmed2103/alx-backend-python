cc97d17571ff21760aae328a433db48651113e8f
hasona <bekoahmed20@gmail.com>
2025-09-30 17:07:29 +0300

    cleanup

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,15 +1,14 @@
-from rest_framework.views import APIView
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from rest_framework import status, permissions
 from django.contrib.auth import logout
 
-class DeleteUserView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
-
-    def delete(self, request):
-        user = request.user
-        user.delete()
-        logout(request)
-        return Response({"message": "User deleted successfully"},status=status.HTTP_204_NO_CONTENT)
+@api_view(['DELETE'])
+@permission_classes([permissions.IsAuthenticated])
+def delete_user(self, request):
+    user = request.user
+    user.delete()
+    logout(request)
+    return Response({"message": "User deleted successfully"},status=status.HTTP_204_NO_CONTENT)
 
 
